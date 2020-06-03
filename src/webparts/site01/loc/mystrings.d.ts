9a43fe418cae1eefae1449ede43da647f5c3e91d
DangPV <dangpv.fxp@outlook.com>
2020-06-03 10:02:52 +0700

    Init

---
+++ src/webparts/site01/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISite01WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Site01WebPartStrings' {
+  const strings: ISite01WebPartStrings;
+  export = strings;
+}
